141557a8369e087c67a0331201a8faf8e6f4db89
GarrettFadul <garrettfadul@gmail.com>
2023-04-21 13:12:03 -0700

    addFiles

---
+++ Bad Bank 2/home.js
@@ -0,0 +1,11 @@
+function Home(){
+  return (
+    <Card
+      txtcolor="black"
+      header="Welcome!"
+      title="Welcome to bad bank!"
+      text="Feel free to move around using the navigation bar."
+      body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
+    />    
+  );  
+}
